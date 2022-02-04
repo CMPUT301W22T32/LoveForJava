aff9023ff4cb93c676d833da3d108ff8e12cfa6f
JeffDongYang <98992435+JeffDongYang@users.noreply.github.com>
2022-02-03 20:19:00 -0700

    Update Triangle.java

--- app/src/main/java/com/example/loveforjava/Triangle.java
+++ app/src/main/java/com/example/loveforjava/Triangle.java
@@ -1,6 +1,6 @@
 package com.example.loveforjava;
 
-public class Triangle extends Shape{
+public abstract class Triangle extends Shape{
     protected int x;
     protected int y;
     String color = "blue";
