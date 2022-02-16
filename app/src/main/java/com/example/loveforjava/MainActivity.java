85d9e1da2f512123276aec281e44535e11d7177a
Nat Bekele <Bekele@ualberta.ca>
2022-02-15 19:28:24 -0700

    ...

--- app/src/main/java/com/example/loveforjava/MainActivity.java
+++ app/src/main/java/com/example/loveforjava/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.loveforjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText username,
+            useremail;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        useremail = findViewById(R.id.useremail);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                databaseReference.child("login").addValueEventListener(new ValueEvent)
+            }
+        });
     }
 }
