66c00752bd4d1e9b7f475d299c2f1084c5068efd
JeffDongYang <ydong4@ualberta.ca>
2022-03-29 01:08:02 -0600

    123

---
+++ app/src/main/java/com/example/loveforjava/rankingActivity.java
@@ -0,0 +1,15 @@
+package com.example.loveforjava;
+
+import org.w3c.dom.CDATASection;
+
+public class rankingActivity {
+    arraylist<>
+    listview;
+    arrayadapter
+
+
+
+
+    arraylist put data;
+    
+}
