f8a5eb640d5363af470385d4b568b5484762ecb9
LAPTOP-J91SS9VT\jaspe <jaspereng00@gmail.com>
2022-04-03 21:10:46 -0600

    merged from master

--- app/src/main/java/com/example/loveforjava/Rank_Activity.java
+++ app/src/main/java/com/example/loveforjava/Rank_Activity.java
@@ -6,14 +6,37 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Rank_Activity extends AppCompatActivity {
+    ArrayList<String> usernames;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank);
         Intent i = getIntent();
         Player p = (Player) i.getSerializableExtra("player");
+
+        usernames =  new ArrayList<>();
+        ArrayList<String> values =  new ArrayList<>();
+
+        APIMain APIserver = new APIMain();
+        APIserver.getRank(p, "highestCode", new ResponseCallback() {
+            @Override
+            public void onResponse(Map<String, Object> response) {
+                if( (Boolean) response.get("success")){
+                    usernames.addAll( (ArrayList<String>) response.get("usernames"));
+                    values.addAll( (ArrayList<String>) response.get("values"));
+                }else{
+                    //err
+                }
+            }
+        });
+    }
+
+    private void onDataChange(){
+        usernames.addAll( (ArrayList<String>) response.get("usernames"));
     }
 }
